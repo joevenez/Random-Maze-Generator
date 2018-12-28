94374de9598f3fd0f73498e5ec95b6caf6d0a48b
Joseph Veneziano <joseph.veneziano@uoit.net>
2018-12-28 00:07:28 -0500

    Functional maze generator

--- sketch.js
+++ sketch.js
@@ -7,21 +7,25 @@ let bHeight;
 let numOfHoriz;
 let numOfVert;
 let stack = [];
+let complete = false;
 
 //Initial startup function
 function setup(){
-    
     createCanvas(width + 1, height + 1);
     start();
 }
 
 //Start function
 function start(){
-    numOfHoriz = 10;
-    numOfVert = 10;
+    numOfHoriz = 40;
+    numOfVert = 40;
     bWidth = Math.floor(width / numOfHoriz);
     bHeight = Math.floor(height / numOfVert);
     init2DArray(arr, numOfHoriz, numOfVert);
+    //Initialize cursor
+    cursor = new Cursor(0,0);
+    stack.push(cursor);
+    arr[0][0].visited = true;
 }
 
 
@@ -33,12 +37,20 @@ function draw(){
             drawBlock(i,j,arr[i][j]);
         }
     }
+    //Cursor functionality
+    if(!complete){
+        //Draw cursor block
+        fill(255,255,0);
+        rect(cursor.x * bWidth + 1, cursor.y * bHeight + 1, bWidth - 1, bHeight - 1);
+        generate(cursor.x, cursor.y);
+    }
+    
 }
 
 //Create Array to represent maze grid
 function init2DArray(arr, w, h){
     for(let i = 0; i < w; i++){
-        arr.push([])
+        arr.push([]);
         for(let j = 0; j < h; j++){
             arr[i].push(new Cell(j,i));
         }
@@ -63,7 +75,74 @@ function drawBlock(i , j, block){
     if(block.right){
         line(i * bWidth + bWidth, j * bHeight, i * bWidth + bWidth, j * bHeight + bHeight);
     }
-    
+}
+
+function generate(curX, curY){
+    temp = [];
+    //Bounds check 
+    //Above
+    if(curY - 1 >= 0){
+        temp.push([curX, curY - 1]);
+    }
+    //below
+    if(curY + 1 < arr[0].length){
+        temp.push([curX, curY + 1]);
+    }
+    //left
+    if(curX - 1 >= 0){
+        temp.push([curX - 1, curY]);
+    }
+    //right
+    if(curX + 1 < arr.length){
+        temp.push([curX + 1, curY]);
+    }
+    //visited check
+    for(let i = temp.length - 1; i >= 0; i--){
+        if(arr[temp[i][0]][temp[i][1]].visited){
+            temp.splice(i,1);
+        }
+    }
+    //pop if all neighbours visited 
+    if(temp.length < 1){
+        //move back to last position
+        stack.pop();
+        cursor = stack[stack.length - 1];
+        // cursor.x = stack[stack.length -1][0];
+        // cursor.y = stack[stack.length -1][1];
+        if(stack.length < 1){
+            complete = true;
+        }
+        return 0;
+    }else{
+        //update cursor to new position
+        let ind = Math.floor(Math.random() * temp.length);
+        cursor = new Cursor(temp[ind][0], temp[ind][1]);
+        stack.push(cursor);
+        arr[cursor.x][cursor.y].visited = true; 
+
+        //remove barriers
+        if(curX - cursor.x == 0){
+            //if y updated
+            if(curY < cursor.y){
+                arr[curX][curY].bot = false;
+                arr[cursor.x][cursor.y].top = false;
+            }else{
+                arr[curX][curY].top = false;
+                arr[cursor.x][cursor.y].bot = false;
+            }
+        }
+        
+        if(curY - cursor.y == 0){
+            //if x updated
+            if(curX < cursor.x){
+                arr[curX][curY].right = false;
+                arr[cursor.x][cursor.y].left = false;
+            }else{
+                arr[curX][curY].left = false;
+                arr[cursor.x][cursor.y].right = false;
+            }
+        }
+    }    
 }
 
 //Cell constructor
@@ -75,4 +154,10 @@ function Cell(x,y){
     this.right = true;
     this.top = true;
     this.bot = true;
+}
+
+//Cursor
+function Cursor(x,y){
+    this.x = x;
+    this.y = y;
 }
